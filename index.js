74c8ffeb56ecc524cbed525862f7bf21d8f71abf
Moist_Nacho <hajo1563@gmail.com>
2021-03-13 15:55:03 +0900

    Protected information

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hajo1563:abcd123@boiler.h86hs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+const config = require('./config/key');
+
+const bodyParser = require('body-parser');
+const {User} = require('./models/User');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify:false
 }).then(()=>console.log('MongoDB Connected...'))
   .catch(error => console.log(error))
 
 app.get('/', (req, res) => {
-  res.send('Hello 준성!')
+  res.send('Hello 준성!2')
+})
+
+app.post('/register', (req, res) => {
+  // 회원가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+
+  const user = new User(req.body);
+  user.save((err, userInfo)=>{
+    if(err) return res.json({success:false, err});
+    return res.status(200).json({
+      success: true,
+      userInfo
+    });
+  });
 })
 
 app.listen(port, () => {
